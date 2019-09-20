3250400e329426d54b503c3fd93ab273a5958080
Aziz Zakiryarov <aziz.zakiryarov@technia.com>
2019-09-20 21:38:46 +0200

    Added UserService Business logic

--- src/main/java/se/azza/userservice/resources/UserResources.java
+++ src/main/java/se/azza/userservice/resources/UserResources.java
@@ -1,4 +1,4 @@
-package se.azza.userservice.services;
+package se.azza.userservice.resources;
 
 import java.util.List;
 import java.util.Optional;
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,56 +19,60 @@ import org.springframework.web.bind.annotation.RestController;
 import se.azza.userservice.constants.States.userState;
 import se.azza.userservice.model.User;
 import se.azza.userservice.repository.UserRepository;
+import se.azza.userservice.services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserResources {
+	
+	@Autowired
+	UserRepository userRepository;
 
 	@Autowired
-	private UserRepository userRepository;
+	UserService userService;
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@RequestMapping("/add")
 	public ResponseEntity<User> addUser(@RequestParam(value = "firstName") String firstName,
 			@RequestParam(value = "lastName") String lastName, @RequestParam(value = "userName") String userName,
 			@RequestParam(value = "password") String password) {
 
 		User newUser = new User(firstName, lastName, userName, password);
-		userRepository.save(newUser);
+		userService.createUser(newUser);
 		return new ResponseEntity<User>(newUser, HttpStatus.CREATED);
 	}
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping(path = "/get/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> getUserById(@PathVariable(value = "id") Long id) {
-		Optional<User> userId = userRepository.findById(id);
+		Optional<User> userId = userService.getUserById(id);
 		return new ResponseEntity<User>(userId.get(), HttpStatus.OK);
 	}
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@PutMapping(value = "/update/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> updateUserById(@PathVariable(value = "id") Long id,
 			@RequestParam(value = "firstName") String firstName, @RequestParam(value = "lastName") String lastName,
 			@RequestParam(value = "userName") String userName, @RequestParam(value = "password") String password,
 			@RequestParam(value = "userState") userState userState) {
+		System.out.println(firstName + ' ' + lastName + ' ' + userName + ' ' + password + ' ' + userState);
 		Optional<User> currentUser = userRepository.findById(id);
-		String INACTIVE = se.azza.userservice.constants.States.userState.INACTIVE.toString();
-		String ACTIVE = se.azza.userservice.constants.States.userState.ACTIVE.toString();
-		if (userState.equals(INACTIVE) || userState.equals(ACTIVE)) {
-			User newUser = new User(currentUser.get().getId(), firstName, lastName, userName, password, userState);
-			userRepository.save(newUser);
-			return new ResponseEntity<User>(newUser, HttpStatus.OK);
-		} else {
-			return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
-		}
+		User newUser = new User(currentUser.get().getId(), firstName, lastName, userName, password, userState);
+		userService.updateUser(newUser);
+		return new ResponseEntity<User>(newUser, HttpStatus.OK);
 	}
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@DeleteMapping(path = "/delete/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<User> deleteUserById(@PathVariable(value = "id") Long id) {
-		userRepository.deleteById(id);
+		userService.deleteById(id);
 		return new ResponseEntity<User>(HttpStatus.OK);
 	}
 
+	@CrossOrigin(origins = "*", allowedHeaders = "*")
 	@GetMapping(path = "/getAll", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<List<User>> getAllUsers() {
-		List<User> users = userRepository.findAll();
+		List<User> users = userService.getAllUsers();
 		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
 	}
 }
---
+++ src/main/java/se/azza/userservice/services/UserService.java
@@ -0,0 +1,37 @@
+package se.azza.userservice.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import se.azza.userservice.model.User;
+import se.azza.userservice.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public User createUser(User user) {
+		return userRepository.save(user);
+	}
+
+	public Optional<User> getUserById(long userId) {
+		return userRepository.findById(userId);
+	}
+
+	public void updateUser(User user) {
+		userRepository.save(user);
+	}
+
+	public void deleteById(Long id) {
+		userRepository.deleteById(id);
+	}
+
+	public List<User> getAllUsers() {
+		return userRepository.findAll();
+	}
+}
