2937a82604176de1b626d9e6c8c8a70240aef616
azizzakiryarov <azizzakiryarov87@gmail.com>
2022-01-31 00:20:32 +0100

    Add shell code to delete and recreate db

---
+++ script/local/microservices-mysql-db/create-deployment-microservices-mysql-db.sh
@@ -0,0 +1,7 @@
+kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/deployment.yaml
+
+kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/service.yaml
+
+kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/persistent-volume.yaml
+
+kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/persistent-volume-claim.yaml
