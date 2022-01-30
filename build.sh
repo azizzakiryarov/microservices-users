e723d6d8367f395345d06a3d236c1317f786819c
azizzakiryarov <azizzakiryarov87@gmail.com>
2022-01-31 00:20:07 +0100

    Add shell code to delete and recreate db

--- build.sh
+++ build.sh
@@ -6,7 +6,17 @@ docker build -f Dockerfile -t azizzakiryarov/microservices-users:latest .
 
 docker push azizzakiryarov/microservices-users:latest
 
+kubectl delete deployment.apps/mysql service/mysql-service
+
 kubectl delete statefulset.apps/microservices-users-deployment service/microservices-users
+
+kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/deployment.yaml
+
+kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/service.yaml
+
+kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/persistent-volume.yaml
+
+kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-mysql-db/persistent-volume-claim.yaml
 
 kubectl apply -f /Users/azizzakiryarov/IdeaProjects/microservices-users/k8s/base/microservices-users/deployment.yaml
 
