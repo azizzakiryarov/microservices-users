798b84ee9b942e1203e60fb7377c4cc8bc21e63a
azizzakiryarov <azizzakiryarov87@gmail.com>
2022-02-23 23:06:20 +0100

    Modified Urls

--- src/main/java/se/azza/userservice/constants/Urls.java
+++ src/main/java/se/azza/userservice/constants/Urls.java
@@ -2,7 +2,7 @@ package se.azza.userservice.constants;
 
 public final class Urls {
 
-    public static final String BASE_URL = "microservices-issues-deployment-0.microservices-issues.microservices.svc.cluster.local:8869";
+    public static final String BASE_URL = "microservices-issues-statefulset-0.microservices-issues.microservices.svc.cluster.local:8869";
 
     public static final String GET_ISSUES_FOR_USER = BASE_URL + "/issues/getAllIssuesFor/";
 
